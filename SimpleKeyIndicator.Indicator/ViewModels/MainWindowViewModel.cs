40aa6b4ad72c3b169ea4d67d537ad894327b0dd7
ParaN3xus <136563585+ParaN3xus@users.noreply.github.com>
2023-08-05 08:12:25 +0800

    MainWindow for debug

--- SimpleKeyIndicator.Indicator/ViewModels/MainWindowViewModel.cs
+++ SimpleKeyIndicator.Indicator/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace SimpleKeyIndicator.Indicator.ViewModels;
+﻿using Avalonia;
+using Avalonia.Controls;
+using SharpHook.Native;
+using SimpleKeyIndicator.Indicator.Controls;
+using SimpleKeyIndicator.Indicator.Views;
+
+namespace SimpleKeyIndicator.Indicator.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public Control[] Keys => new Control[]
+    {
+        new KeyIndicator(MainWindow.MainListener,KeyCode.VcW)
+        {
+            Width = 50,
+            Height = 50,
+            Margin = new Thickness(50,50),
+        }
+    };
+
 }
--- SimpleKeyIndicator.Indicator/Views/MainWindow.axaml.cs
+++ SimpleKeyIndicator.Indicator/Views/MainWindow.axaml.cs
@@ -1,11 +1,17 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using SimpleKeyIndicator.Indicator.Models;
 
 namespace SimpleKeyIndicator.Indicator.Views;
 
 public partial class MainWindow : Window
 {
+    public static KeyListener MainListener = new();
+    
     public MainWindow()
     {
         InitializeComponent();
+        
+        MainListener.Run();
     }
 }
